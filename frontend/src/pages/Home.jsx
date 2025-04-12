5ad2019b2881f16efad3751bdfae525fcb9a9000
shobhitb2204 <shobhitb2204@gmail.com>
2025-04-12 21:00:55 +0530

    feat: Add initial frontend structure with components, pages, and assets
    - Implemented Header and Navbar components for navigation.
    - Created SpecialityMenu component to display specialities.
    - Added pages for Home, About, Appointment, Contact, Doctors, Login, MyAppointments, and MyProfile.
    - Integrated SVG and PNG assets for icons and logos.
    - Configured Tailwind CSS for styling and layout.
    - Set up Vite for development environment.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <SpecialityMenu/>
+    </div>
+  )
+}
+
+export default Home
