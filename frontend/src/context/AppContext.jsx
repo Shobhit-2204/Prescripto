2ce7f3349090fe9ce6e475be5354960af43f12a5
shobhitb2204 <shobhitb2204@gmail.com>
2025-05-29 08:27:19 +0530

    Implement user registration and login functionality with JWT authentication

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,12 +9,11 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [doctors,setDoctors] = useState([])
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [doctors,setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+
+    
 
     const getDoctorsData = async () => {
         try {
@@ -31,6 +30,13 @@ const AppContextProvider = (props) => {
             console.log(error)  
             toast.error(error.message)
         }
+    }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token, setToken,
+        backendUrl
     }
 
     useEffect(() => {
